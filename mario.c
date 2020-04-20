cdff941e4894c88a799786948ab4d3fccfbe715e
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-04-20 11:03:05 +0000

    automated commit by submit50 [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,34 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+    while(true)
+    {
+        n= get_int("Height: ");
+        if(n>0 && n<9)
+        {
+            break;
+        }
+    }
+    int p = n;
+    for( int i = 0 ; i < p; i++)
+    {
+        for(int j = 1; j < n; j++)
+        {
+            printf(" ");
+        }
+        for(int k =0; k<=i; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for(int m = 0; m<=i ; m++ )
+        {
+            printf("#");
+        }
+        printf("\n");
+        n--;
+    }
+}
