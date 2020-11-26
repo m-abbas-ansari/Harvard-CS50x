faa92761cd2fc91ce5de1647076af1d6e84231cf
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-11-26 15:42:03 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -9,33 +9,33 @@ int main(void)
     string s = get_string("Text: ");
     int ct_let = 0, ct_words = 1, ct_sent = 0;
 
-    for(int i = 0, n = strlen(s); i<n ; i++)
+    for (int i = 0, n = strlen(s); i < n ; i++)
     {
-        if(isalpha(s[i]) != 0)                      //isalpha returns non 2zero if alphabetical (Counting Letters)
+        if (isalpha(s[i]) != 0) //isalpha returns non 2zero if alphabetical (Counting Letters)
         {
             ct_let++;
         }
-        if(isspace(s[i]) !=0)                       //isspace returns non zero is white space (Counting Words)
+        if (isspace(s[i]) != 0) //isspace returns non zero is white space (Counting Words)
         {
             ct_words++;
         }
-        if( s[i]==  '.' || s[i]== '!' || s[i]== '?') // Checking if char is '.' or '!' or '?' (Counting Sentences)
+        if (s[i] ==  '.' || s[i] == '!' || s[i] == '?')  // Checking if char is '.' or '!' or '?' (Counting Sentences)
         {
             ct_sent++;
         }
     }
     //Coleman-Liau Formula Application
-    float L,S;
-    L = ((float)ct_let/ (float)ct_words)*100;          //avg. no. of letters per 100 words
-    S = ((float)ct_sent/ (float)ct_words)*100;         //avg. no of sentences per 100 words
-    int index = round( (0.0588*L) - (0.296*S) - 15.8); //Coleman-Liau index
-    if (index<1)
+    float L, S;
+    L = ((float)ct_let / (float)ct_words) * 100; //avg. no. of letters per 100 words
+    S = ((float)ct_sent / (float)ct_words) * 100; //avg. no of sentences per 100 words
+    int index = round((0.0588 * L) - (0.296 * S) - 15.8); //Coleman-Liau index
+    if (index < 1)
     {
         printf("Before Grade 1\n");
     }
-    else if(index<16)
+    else if (index < 16)
     {
-        printf("Grade %i\n",index);
+        printf("Grade %i\n", index);
     }
     else
     {
