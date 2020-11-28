dc589d06589a66ed74445b098bad16c6c1bf062f
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-11-28 17:37:01 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -6,17 +6,19 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
-    int k;
+    float k;
     for(int i = 0 ; i < height; i++)
     {
         for(int j = 0; j < width; j++)
         {
-            k = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed)/3);
-            image[i][j].rgbtBlue = k;
-            image[i][j].rgbtGreen = k;
-            image[i][j].rgbtRed = k;
+            k = ((float)image[i][j].rgbtBlue + (float)image[i][j].rgbtGreen + (float)image[i][j].rgbtRed)/3;
+            image[i][j].rgbtBlue = round(k);
+            image[i][j].rgbtGreen = round(k);
+            image[i][j].rgbtRed = round(k);
         }
+        
     }
+    
 }
 
 // Convert image to sepia
