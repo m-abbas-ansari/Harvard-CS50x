61a257ee0fadb8a6d3b664e2bc1bae4df0bb957e
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-12-03 14:12:43 +0000

    automated commit by submit50

--- cash.py
+++ cash.py
@@ -1,23 +1,17 @@
 from cs50 import get_float
-
+#Getting and storing valid change
 while True:
     cg = (get_float("Change owed: ")) * 100
     if cg > 0 and cg != 0.00:
-        break;
+        break
 
-n = round(cg)
-a = n % 25
-p = (n - a) / 25
+n = round(cg) 
+a = n % 25 
+p = (n - a) / 25 #counting number of 25 cent coins
 b = a % 10
-q = (a - b) / 10
+q = (a - b) / 10 #counting number of 10 cent coins
 c = b % 5
-r = (b - c) / 5
-s = c
-ans = round(p + q + r + s)
+r = (b - c) / 5 #counting number of 5 cent coins
+s = c #number of 1 cent coins
+ans = round(p + q + r + s) #final no of total coins
 print(f"{ans} ")
-
-
-
-
-
-
