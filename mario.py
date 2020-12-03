a9340cbb875ee7ddb2026cdb4244bd4e3f1c0547
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-12-03 14:07:39 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -3,11 +3,11 @@ from cs50 import get_int
 while True:
     n = get_int("Height: ")
     if n > 0 and n < 9 :
-        break;
+        break
 
 for i in range(n):
-    print(" "*(n - (i+1)), end = "")
+    print(" "*(n - (i + 1)), end = "")
     print("#"*(i + 1), end = "")
-    print(" ", end = "")
+    print("  ", end = "")
     print("#"*(i + 1), end = "")
     print()
