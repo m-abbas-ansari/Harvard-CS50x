016275bd47698fdfd375114f634cd0271cb41947
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-12-03 14:15:44 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,15 +1,15 @@
 from cs50 import get_int
-#getting a valid height
+# getting a valid height
 while True:
     n = get_int("Height: ")
-    if n > 0 and n < 9 :
+    if n > 0 and n < 9:
         break
-#printing pyramid
+# printing pyramid
 for i in range(n):
-    #left half
-    print(" "*(n - (i + 1)), end = "")
-    print("#"*(i + 1), end = "")
-    print("  ", end = "")
-    #right half
-    print("#"*(i + 1), end = "")
-    print() # newline
+    # left half
+    print(" "*(n - (i + 1)), end="")
+    print("#"*(i + 1), end="")
+    print("  ", end="")
+    # right half
+    print("#"*(i + 1), end="")
+    print()  # newline
