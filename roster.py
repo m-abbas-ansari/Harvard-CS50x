a73a10aa7a8d5d240317dbf46a970e1c1e1a3e6a
King-of-Haskul <King-of-Haskul@users.noreply.github.com>
2020-12-06 07:20:27 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -2,27 +2,26 @@
 import csv
 import cs50
 from sys import argv
-#Checking for valid command line args
+# Checking for valid command line args
 if len(argv) != 2:
     print("Error")
     exit()
 
-#Opening database
+# Opening database
 db = cs50.SQL("sqlite:///students.db")
 
-#Storing SQL query in a dictionary called query{}
+# Storing SQL query in a dictionary called query{}
 query = {}
 query = db.execute("SELECT first,middle,last,birth FROM students WHERE house = ? ORDER BY last, first", argv[1])
 
-#iterating through dictionary
+# iterating through dictionary
 for row in query:
     first = row["first"]
     middle = row["middle"]
     last = row["last"]
     birth = row["birth"]
-    #Check for middle name
-    if middle == "":
+    # Check for middle name
+    if middle == None:
         print(f"{first} {last}, born {birth}")
     else:
         print(f"{first} {middle} {last}, born {birth}")
-
